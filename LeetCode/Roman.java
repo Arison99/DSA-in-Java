f883d008e7afb57e64a89304c85b51d8c3e3424c
Arison99 <harrisondaviinci@gmail.com>
2025-01-10 11:07:17 +0300

    Implement Roman numeral to integer conversion and add test cases

--- LeetCode/Roman.java
+++ LeetCode/Roman.java
@@ -2,7 +2,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
+    /**
+     * Converts a Roman numeral to an integer.
+     * 
+     * @param s the Roman numeral string
+     * @return the integer representation of the Roman numeral
+     */
     public int romanToInt(String s) {
+        // Create a map to store the Roman numeral values
         Map<Character, Integer> romanMap = new HashMap<>();
         romanMap.put('I', 1);
         romanMap.put('V', 5);
@@ -12,18 +19,32 @@ class Solution {
         romanMap.put('D', 500);
         romanMap.put('M', 1000);
 
-        int result = 0;
-        int length = s.length();
+        int result = 0; // Initialize the result variable
+        int length = s.length(); // Get the length of the input string
 
+        // Iterate through each character in the string
         for (int i = 0; i < length; i++) {
-            int currentVal = romanMap.get(s.charAt(i));
+            int currentVal = romanMap.get(s.charAt(i)); // Get the value of the current Roman numeral
+            // If the current value is less than the next value, subtract it from the result
             if (i < length - 1 && currentVal < romanMap.get(s.charAt(i + 1))) {
                 result -= currentVal;
             } else {
+                // Otherwise, add it to the result
                 result += currentVal;
             }
         }
 
-        return result;
+        return result; // Return the final result
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        // Test cases
+        System.out.println(solution.romanToInt("III")); // Output: 3
+        System.out.println(solution.romanToInt("IV")); // Output: 4
+        System.out.println(solution.romanToInt("IX")); // Output: 9
+        System.out.println(solution.romanToInt("LVIII")); // Output: 58
+        System.out.println(solution.romanToInt("MCMXCIV")); // Output: 1994
     }
 }
