13be0d74c5b36307cea5c0cfbc1276783024ed00
Arison99 <harrisondaviinci@gmail.com>
2025-03-17 21:22:59 +0300

    Updates

--- LeetCode/Remove Elements/RemoveElements.java
+++ LeetCode/Remove Elements/RemoveElements.java
@@ -9,4 +9,27 @@ public class RemoveElements {
         }
         return k;
     }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val = 2;
+        RemoveElements re = new RemoveElements();
+        int k = re.removeElement(nums, val);
+        System.out.println("Number of elements not equal to " + val + ": " + k);
+        System.out.print("Modified array: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
 }
+
+/*
+ * Pseudocode:
+ * 1. Initialize a counter k to 0.
+ * 2. Iterate through each element in the array.
+ * 3. If the current element is not equal to val, assign it to nums[k] and increment k.
+ * 4. Return k, the count of elements not equal to val.
+ *
+ * Time Complexity: O(n) - We iterate through the array once.
+ * Space Complexity: O(1) - We use a constant amount of extra space.
+ */
