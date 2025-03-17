b4dcc5aca6138fe883d68d459cf4a095ea828139
Arison99 <harrisondaviinci@gmail.com>
2025-03-17 21:20:38 +0300

    Updates

---
+++ LeetCode/Remove Elements/RemoveElements.java
@@ -0,0 +1,12 @@
+public class RemoveElements {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; // Count of elements not equal to val
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
